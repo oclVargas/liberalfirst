c12b4aaec0054b1f6765bbb8d18680b73996153b
Osiel <ocl.vargasanchez@gmail.com>
2020-03-29 16:15:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <h1>Man Arrested For Stealing Bread Over 6 Years Ago</h1>
+      <span className="article-date">Sunday 3:16PM • LIBERAL</span>
+      <img className="bread-image" src="./bread.jpg" alt=""/>
+      <article>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LIBERAL--A Liberal man has been arrested for stealing bolillos from Dillons in 2014.
+          After a lengthy investigation by Liberal Police Department, the suspect, Osiel Vargas-Sanchez, was arrested in his home without incident.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Sanchez, an employee of Dillons at the time, can be seen in new unreleased footage stuffing plastic bags full of
+          bread. The photo as seen above was also published on Sanchez's Facebook page. An anonymous tip led to his arrest later that
+          day.
+        </p>
+        <p>
+          A statement released from Sanchez says that he did nothing wrong. Noting that it was an error by
+          an employee that caused the bread to be so cheap. As part of a 'Buy 5 save $5' deal, the bags of bread
+          could be bought 5 at a time so that the discount would be $5 thus equaling $0. 'Free bread', he said.
+        </p>
+      </article>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <img className='logo' src='logo.jpg' alt="logo"/>
+    )
+}
+
+export default Header
